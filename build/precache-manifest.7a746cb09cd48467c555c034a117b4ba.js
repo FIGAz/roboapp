1da5616a9c6a8c797ff960022463d57a66c06853
FIGAz <49418613+FIGAz@users.noreply.github.com>
2019-11-30 03:32:20 +0300

    Add files via upload

---
+++ build/precache-manifest.7a746cb09cd48467c555c034a117b4ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed8a9e829293abb7e17de2e1a585024",
+    "url": "/index.html"
+  },
+  {
+    "revision": "002450d58634f9aca5e2",
+    "url": "/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "4cf13608a9f60ef37e89",
+    "url": "/static/css/main.ae2b5273.chunk.css"
+  },
+  {
+    "revision": "002450d58634f9aca5e2",
+    "url": "/static/js/2.4a61627c.chunk.js"
+  },
+  {
+    "revision": "4cf13608a9f60ef37e89",
+    "url": "/static/js/main.91174178.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
